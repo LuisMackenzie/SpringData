32e4d2b156e4bf9c009dff88e63d15e20aadb9c7
Mackenzie <principemackenzie@gmail.com>
2021-09-02 15:17:44 +0200

    Data Layer Impl

---
+++ src/main/java/com/mackenzie/SpringData/SpringData.java
@@ -0,0 +1,14 @@
+package com.mackenzie.SpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringData {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringData.class, args);
+		//System.out.println("Hello world MAckenzie Consolas");
+	}
+
+}
